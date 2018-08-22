9a65ea3f3b8d56cfcec4056798f23498fefc4809
top1st <riguang102@gmail.com>
2018-08-22 20:42:32 +0800

    remove compose

--- get-docker.sh
+++ get-docker.sh
@@ -523,6 +523,3 @@ do_install() {
 # wrapped up in a function so that we have some protection against only getting
 # half the file during "curl | sh"
 do_install
-
-curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
